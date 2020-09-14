71d97281c819d7bc54513a215223b8c7f9e0ecbe
jhunderdog <33382water@gmail.com>
2020-09-14 20:44:04 +0900

    Follow Button

--- src/Components/Routes.js
+++ src/Components/Routes.js
@@ -9,21 +9,24 @@ import Profile from "../Routes/Profile";
 
 const LoggedInRoutes = () => (
   <Switch>
-    <Route exact path="/" component={Feed}></Route>
-    <Route exact path="/" component={Explore}></Route>
-    <Route exact path="/" component={Search}></Route>
-    <Route exact path="/" component={Profile}></Route>
+    <Route exact path="/" component={Feed} />
+    <Route path="/explore" component={Explore} />
+    <Route path="/search" component={Search} />
+    <Route path="/:username" component={Profile} />
   </Switch>
 );
+
 const LoggedOutRoutes = () => (
   <Switch>
     <Route exact path="/" component={Auth} />
   </Switch>
 );
+
 const AppRouter = ({ isLoggedIn }) =>
   isLoggedIn ? <LoggedInRoutes /> : <LoggedOutRoutes />;
 
 AppRouter.propTypes = {
   isLoggedIn: PropTypes.bool.isRequired,
 };
+
 export default AppRouter;
